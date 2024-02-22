3f0d8f4a9d7ed88e9e5b86d81b3e4ade518d2c34
RudolfxJ <rudolfxjacobs@gmail.com>
2024-02-22 09:34:19 +0200

    [feat] added basic seed management command script

---
+++ backend/Employee/management/commands/seed.py
@@ -0,0 +1,72 @@
+from django.core.management.base import BaseCommand
+from Employee.models import Employee, Skill
+
+class Command(BaseCommand):
+    employees = [
+        {
+        "first_name": "John",
+        "last_name": "Doe",
+        "contact_number": "1234567890",
+        "email": "qOqQH@example.com",
+        "date_of_birth": "1990-01-01",
+        "street_address": "123 Any Street",
+        "city": "Anytown",
+        "postal_code": "12345",
+        "country": "USA",
+        "skills": [
+            {
+            "name": "Programming",
+            "years_experience": "5",
+            "seniority_rating": "Senior",
+            },
+        ],
+        },
+        {
+        "first_name": "Jane",
+        "last_name": "Ploe",
+        "contact_number": "0987654321",
+        "email": "aBaBZ@example.com",
+        "date_of_birth": "1992-02-02",
+        "street_address": "456 Any Avenue",
+        "city": "Anytown",
+        "postal_code": "54321",
+        "country": "USA",
+        "skills": [
+            {
+            "name": "Design",
+            "years_experience": "3",
+            "seniority_rating": "Junior",
+            },
+        ],
+        },
+        {
+        "first_name": "Jim",
+        "last_name": "Koe",
+        "contact_number": "1122334455",
+        "email": "xYxYZ@example.com",
+        "date_of_birth": "1994-03-03",
+        "street_address": "789 Any Boulevard",
+        "city": "Anytown",
+        "postal_code": "67890",
+        "country": "USA",
+        "skills": [
+            {
+            "name": "Project Management",
+            "years_experience": "2",
+            "seniority_rating": "Intermediate",
+            },
+        ],
+        },
+    ]
+
+    def handle(self, *args, **kwargs):
+        Employee.objects.all().delete()
+        for employee in self.employees:
+            data = employee
+            skills = data.pop("skills")
+            employee_obj = Employee.objects.create(**employee)
+            for skill in skills:
+                skill['employee'] = employee_obj
+                Skill.objects.create(**skill)
+
+        return
